0f45946f9f22e059f356ca8d3c97e16954faffb7
AndrePogi <lacraandre111@gmail.com>
2023-06-17 13:04:26 +0800

    initialCommit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ReizTech_InternAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
