b8afff816d8d35e5005a8dfcad48beda2cacc36e
AndrePogi <lacraandre111@gmail.com>
2023-06-17 17:12:40 +0800

    finished problem 1

--- obj/Debug/net8.0/ReizTech_InternAssignment.AssemblyInfo.cs
+++ obj/Debug/net8.0/ReizTech_InternAssignment.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReizTech_InternAssignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f45946f9f22e059f356ca8d3c97e16954faffb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReizTech_InternAssignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReizTech_InternAssignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
